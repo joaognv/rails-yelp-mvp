29d4a0aa40ee79064a4eaa38168f301d1b1ada44
Joao Viana <jguilherme_viana@hotmail.com>
2018-02-28 17:24:18 +0100

    rail-yelp 5.30pm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create name: 'El Taco', phone_number: '08 898 4234', address: "El numero 2", category: "italian"
+Restaurant.create name: 'Pizza Pizza', phone_number: '08 88988 434', address: "La numero uno", category: "french"
